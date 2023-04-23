95cd69d3ca8034b83489c96e841ba8c973a7691a
Antheagao <Anthonymendez035@gmail.com>
2023-04-22 22:55:12 -0700

    created hello systemcall

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -89,3 +89,12 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+// hello syscall definition
+uint64 sys_hello(void)
+{
+  int n;
+  argint(0, &n);
+  print_hello(n);
+  return 0;
+}
--- user/usys.pl
+++ user/usys.pl
@@ -36,3 +36,4 @@ entry("getpid");
 entry("sbrk");
 entry("sleep");
 entry("uptime");
+entry("hello"); # hello syscall for user
